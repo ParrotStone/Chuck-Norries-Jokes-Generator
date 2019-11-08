293b787cc9c0a7cc18d41a0492d19378d3a9738a
Abdulrahman Ali Hussain <ParrotStone@gmail.com>
2019-11-08 11:42:36 +0200

    Update main.js
    Modified the link to get the resources(jokes) from the secure channel(https).

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function loadJokes(event) {
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+    xhr.open('GET', `https://api.icndb.com/jokes/random/${number}`, true);
 
     xhr.onload = function () {
         if (this.status == '200') {
